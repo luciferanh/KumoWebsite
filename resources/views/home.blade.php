9b58afd2adf30e7bff0783a870e72e18a86f0add
phuong <nguyenphuonghoang14200@gmail.com>
2021-10-01 01:17:37 -0700

    silder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,8 @@
+
 @include('nav')
+@include('silder')
+<div>
+    <p> NEW ARRIVAL</p>
+    <img src="PNG/1.jpg" alt="">
+    <img src="PNG/1.jpg" alt="">
+</div>
---
+++ resources/views/silder.blade.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" type="text/css" href="/admin/css/slider.css">
+<div class="slide1">
+    <div class="img-slider">
+      <div class="slide active">
+        <img class="hinhanh" src="PNG/logo.png" alt="">
+      </div>
+      <div class="slide">
+        <img class="hinhanh" src="PNG/1.jpg" alt="">
+      </div>
+      <div class="slide">
+        <img class="hinhanh" src="PNG/Kumo.png" alt="">
+      </div>
+      <div class="slide">
+        <img class="hinhanh" src="PNG/mangnhen.png" alt="">
+      </div>
+      <div class="slide">
+        <img class="hinhanh" src="PNG/logo.png" alt="">
+      </div>
+      <div class="navigation">
+        <div class="btn active"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+      </div>
+    </div>
+</div>
+<script src="js\slider.js"></script>
